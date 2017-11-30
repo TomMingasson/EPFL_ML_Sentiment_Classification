3ad3b7d0692be3829256bc55de2d30e4763483ed
Tom Mingasson <tommingasson@hotmail.fr>
2017-11-30 11:57:32 +0100

    ok

--- code/helpers/pickle_vocab.py
+++ code/helpers/pickle_vocab.py
@@ -3,7 +3,9 @@ import pickle
 
 
 def main():
+    
     vocab = dict()
+    
     with open('vocab_cut.txt') as f:
         for idx, line in enumerate(f):
             vocab[line.strip()] = idx
@@ -11,6 +13,7 @@ def main():
     with open('vocab.pkl', 'wb') as f:
         pickle.dump(vocab, f, pickle.HIGHEST_PROTOCOL)
 
+    print('pickle_vocab.py executed')
 
 if __name__ == '__main__':
     main()
